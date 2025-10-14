43c4d63cc433da756bbd68f486f0d5f224949200
SrujanRP2006 <190084678+SrujanRP2006@users.noreply.github.com>
2025-10-14 19:24:19 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+name = input("his name is: ")
+if name == "srujan":
+    print("free food in hostel")
+else:
+    if name =="Rahul":
+        print("free food in hostel")
+    elif name =="jagan":
+        print("no free food in hostel")
+    elif name =="sumanth":
+        print("my be free food because he is srujan's brother")
+    else:
+        if name == "charan":
+            print("he has free food because he paied the fees")
+    
